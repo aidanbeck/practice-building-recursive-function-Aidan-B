1204252f8d3d7597bbc15316534e069b02c1db8f
aidanbeck <aidanmbeck@outlook.com>
2025-04-22 18:43:06 -0500

    recursion2

--- reverseString.js
+++ reverseString.js
@@ -7,7 +7,15 @@ function reverseString(string) {
     let lastLetter = string[string.length-1];
     let stringMinusLastLetter = string.slice(0, string.length-1);
 
+    /*
+        This function saves the last letter, then removes it from the string.
+        Then, it "reverses" the rest of the string by resursively calling this function again.
+        Each time the function is called, it pulls a letter from the end of the string to the beginning of the remaining string.
+        When it is done, the string has been reversed.
+    */
+
     return lastLetter + reverseString(stringMinusLastLetter);
+
 }
 
 console.log( reverseString("hello")     ); // Outputs: "olleh" 
