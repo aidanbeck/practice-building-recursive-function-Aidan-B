e9d8cd7efcf00d7578d09c95caeb231bd72f0782
aidanbeck <aidanmbeck@outlook.com>
2025-04-24 18:42:35 -0500

    recursion

--- reverseString.js
+++ reverseString.js
@@ -1,24 +1,16 @@
 function reverseString(string) {
-    
-    if (string.length <= 1) {
-        return string;
+
+    if (string.length == 1 || string.length == 0) {
+        return string; //string's reverse is identical
     }
 
     let lastLetter = string[string.length-1];
-    let stringMinusLastLetter = string.slice(0, string.length-1);
+    let remainingLetters = string.slice(0, string.length-1);
 
-    /*
-        This function saves the last letter, then removes it from the string.
-        Then, it "reverses" the rest of the string by resursively calling this function again.
-        Each time the function is called, it pulls a letter from the end of the string to the beginning of the remaining string.
-        When it is done, the string has been reversed.
-    */
-
-    return lastLetter + reverseString(stringMinusLastLetter);
+    return lastLetter + reverseString(remainingLetters);
 
 }
 
-console.log( reverseString("hello")     ); // Outputs: "olleh" 
-console.log( reverseString("recursion") ); // Outputs: "noisrucer" 
-console.log( reverseString("a")         ); // Outputs: "a" 
-console.log( reverseString("")          ); // Outputs: "" 
+let test = reverseString("earth");
+
+console.log(test);
